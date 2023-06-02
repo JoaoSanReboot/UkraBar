3f7ef463344d447f64eaca7966ba9ee9265cc622
Clarakobay <clarakobaydasilva@gmail.com>
2023-06-02 17:24:41 -0300

    funcionario

---
+++ UkraBar-main/UkraBar/PainelFuncionario.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace UkraBar
+{
+    partial class PainelFuncionario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pCadastro = new System.Windows.Forms.Panel();
+            this.BtnProdutos = new System.Windows.Forms.Button();
+            this.pMenu = new System.Windows.Forms.Panel();
+            this.BtnVoltarADM = new System.Windows.Forms.Button();
+            this.BtnClientesView = new System.Windows.Forms.Button();
+            this.BtnCadastrar = new System.Windows.Forms.Button();
+            this.pbFundoAdm = new System.Windows.Forms.PictureBox();
+            this.pCadastro.SuspendLayout();
+            this.pMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFundoAdm)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pCadastro
+            // 
+            this.pCadastro.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.pCadastro.Controls.Add(this.BtnProdutos);
+            this.pCadastro.Location = new System.Drawing.Point(301, 98);
+            this.pCadastro.Margin = new System.Windows.Forms.Padding(4);
+            this.pCadastro.Name = "pCadastro";
+            this.pCadastro.Size = new System.Drawing.Size(249, 78);
+            this.pCadastro.TabIndex = 11;
+            this.pCadastro.Visible = false;
+            // 
+            // BtnProdutos
+            // 
+            this.BtnProdutos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.BtnProdutos.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnProdutos.FlatAppearance.BorderSize = 0;
+            this.BtnProdutos.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(210)))), ((int)(((byte)(2)))));
+            this.BtnProdutos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnProdutos.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnProdutos.ForeColor = System.Drawing.SystemColors.Control;
+            this.BtnProdutos.Location = new System.Drawing.Point(0, 0);
+            this.BtnProdutos.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnProdutos.Name = "BtnProdutos";
+            this.BtnProdutos.Size = new System.Drawing.Size(249, 76);
+            this.BtnProdutos.TabIndex = 6;
+            this.BtnProdutos.Text = "Produtos";
+            this.BtnProdutos.UseVisualStyleBackColor = false;
+            this.BtnProdutos.Click += new System.EventHandler(this.BtnProdutos_Click);
+            // 
+            // pMenu
+            // 
+            this.pMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.pMenu.Controls.Add(this.BtnVoltarADM);
+            this.pMenu.Controls.Add(this.BtnClientesView);
+            this.pMenu.Controls.Add(this.BtnCadastrar);
+            this.pMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pMenu.Location = new System.Drawing.Point(0, 0);
+            this.pMenu.Margin = new System.Windows.Forms.Padding(4);
+            this.pMenu.Name = "pMenu";
+            this.pMenu.Size = new System.Drawing.Size(1371, 98);
+            this.pMenu.TabIndex = 10;
+            // 
+            // BtnVoltarADM
+            // 
+            this.BtnVoltarADM.BackColor = System.Drawing.Color.Crimson;
+            this.BtnVoltarADM.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnVoltarADM.FlatAppearance.BorderSize = 0;
+            this.BtnVoltarADM.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.BtnVoltarADM.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnVoltarADM.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnVoltarADM.ForeColor = System.Drawing.SystemColors.Control;
+            this.BtnVoltarADM.Location = new System.Drawing.Point(41, 18);
+            this.BtnVoltarADM.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnVoltarADM.Name = "BtnVoltarADM";
+            this.BtnVoltarADM.Size = new System.Drawing.Size(153, 62);
+            this.BtnVoltarADM.TabIndex = 9;
+            this.BtnVoltarADM.Text = "Voltar";
+            this.BtnVoltarADM.UseVisualStyleBackColor = false;
+            this.BtnVoltarADM.Click += new System.EventHandler(this.BtnVoltarADM_Click);
+            // 
+            // BtnClientesView
+            // 
+            this.BtnClientesView.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.BtnClientesView.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnClientesView.FlatAppearance.BorderSize = 0;
+            this.BtnClientesView.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.BtnClientesView.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnClientesView.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnClientesView.ForeColor = System.Drawing.SystemColors.Control;
+            this.BtnClientesView.Location = new System.Drawing.Point(589, 20);
+            this.BtnClientesView.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnClientesView.Name = "BtnClientesView";
+            this.BtnClientesView.Size = new System.Drawing.Size(249, 78);
+            this.BtnClientesView.TabIndex = 2;
+            this.BtnClientesView.Text = "Clientes";
+            this.BtnClientesView.UseVisualStyleBackColor = false;
+            this.BtnClientesView.Click += new System.EventHandler(this.BtnClientesView_Click);
+            // 
+            // BtnCadastrar
+            // 
+            this.BtnCadastrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.BtnCadastrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnCadastrar.FlatAppearance.BorderSize = 0;
+            this.BtnCadastrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.BtnCadastrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnCadastrar.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCadastrar.ForeColor = System.Drawing.SystemColors.Control;
+            this.BtnCadastrar.Image = global::UkraBar.Properties.Resources.expand_arrow_24px;
+            this.BtnCadastrar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BtnCadastrar.Location = new System.Drawing.Point(301, 20);
+            this.BtnCadastrar.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnCadastrar.Name = "BtnCadastrar";
+            this.BtnCadastrar.Padding = new System.Windows.Forms.Padding(0, 0, 20, 0);
+            this.BtnCadastrar.Size = new System.Drawing.Size(249, 78);
+            this.BtnCadastrar.TabIndex = 0;
+            this.BtnCadastrar.Text = "  Visualizar";
+            this.BtnCadastrar.UseVisualStyleBackColor = false;
+            this.BtnCadastrar.Click += new System.EventHandler(this.BtnCadastrar_Click);
+            // 
+            // pbFundoAdm
+            // 
+            this.pbFundoAdm.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbFundoAdm.Image = global::UkraBar.Properties.Resources.TelaFundo;
+            this.pbFundoAdm.Location = new System.Drawing.Point(0, 0);
+            this.pbFundoAdm.Margin = new System.Windows.Forms.Padding(4);
+            this.pbFundoAdm.Name = "pbFundoAdm";
+            this.pbFundoAdm.Size = new System.Drawing.Size(1371, 750);
+            this.pbFundoAdm.TabIndex = 12;
+            this.pbFundoAdm.TabStop = false;
+            // 
+            // PainelFuncionario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1371, 750);
+            this.Controls.Add(this.pCadastro);
+            this.Controls.Add(this.pMenu);
+            this.Controls.Add(this.pbFundoAdm);
+            this.Name = "PainelFuncionario";
+            this.Text = "PainelFuncionario";
+            this.pCadastro.ResumeLayout(false);
+            this.pMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbFundoAdm)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pCadastro;
+        private System.Windows.Forms.Button BtnProdutos;
+        private System.Windows.Forms.Panel pMenu;
+        private System.Windows.Forms.Button BtnVoltarADM;
+        private System.Windows.Forms.Button BtnClientesView;
+        private System.Windows.Forms.Button BtnCadastrar;
+        private System.Windows.Forms.PictureBox pbFundoAdm;
+    }
+}
---
+++ UkraBar-main/UkraBar/PainelFuncionario.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UkraBar
+{
+    public partial class PainelFuncionario : Form
+    {
+        public PainelFuncionario()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCadastrar_Click(object sender, EventArgs e)
+        {
+            if (!pCadastro.Visible)
+
+                pCadastro.Visible = true; //Exibi o Painel de Botões 
+
+            else
+
+                pCadastro.Visible = false;//Esconde o Painel de Botões
+        }
+
+        private void BtnProdutos_Click(object sender, EventArgs e)
+        
+            {
+                CadastrarProd ProdCad = new CadastrarProd();
+                this.Hide();
+                ProdCad.ShowDialog();
+                this.Close();
+
+            }//Botão de Cadastro de Produto
+
+        private void BtnClientesView_Click(object sender, EventArgs e)
+        {
+            PainelCliente Cliente = new PainelCliente();
+            this.Hide();
+            Cliente.ShowDialog();
+            this.Close();
+        }
+
+        private void BtnVoltarADM_Click(object sender, EventArgs e)
+        {
+            PainelLogins PainelLog = new PainelLogins();
+            this.Hide();
+            PainelLog.ShowDialog();
+            this.Close();
+        }//Botão de Voltar para Logins
+    }
+    }
+    
+
